dbc2d6156bb370d2392a3eb14ac53d733f2e74b3
Guilherme-Git-Program <g.maia.navegador.directory@gmail.com>
2025-05-04 18:22:35 +0000

    conclusão final do desafio 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+// Tema 2 - Calculando Densidade Populacional e PIB per Capita
 
 int main() {
+
 //Declaração de variaveis
+
     char estado1 = 'A';
-    char codigo1[3] = "01";
+    char codigo1[5] = "01";
     char cidade1[50] = "Paris";
     int habitantes1 = 12325000;
     float area1 = 1521.11;
     float pib1 = 699280000000;
     int turisiticos1 = 50;
-//comecei uma nova declaração pois não consegui resolver o problema do cadastro com uma só
+    float pibpercapta1 = 1;
+    float densidadepopulacional1 = 1;
+
+//segunda declaração de variaveis
+
     char estado2 = 'A';
-    char codigo2[3] = "01";
+    char codigo2[5] = "01";
     char cidade2[50] = "Paris";
     int habitantes2 = 12325000;
     float area2 = 1521.11;
     float pib2 = 699280000000;
     int turisiticos2 = 50;
-    
+    float pibpercapta2 = 1;
+    float densidadepopulacional2 = 1;
+
 //codigo referente a entrada de informações da carta 1
 
     printf("Cadastro da Carta 1: \n");
@@ -69,18 +77,30 @@ int main() {
     scanf("%f", &pib2);
 
     printf("Digite o numero de pontos turisticos: \n");
-    scanf("%d", &turisiticos2);
+    scanf("%d", &turisiticos2); 
+
+//criar a funçao de media da carta 1
+
+    densidadepopulacional1 = habitantes1 / area1 ;
+    pibpercapta1 = pib1 / habitantes1 ;
+
+//criar a funçao de media da carta 2
+
+    densidadepopulacional2 = habitantes2 / area2 ;
+    pibpercapta2 = pib2 / habitantes2 ;
 
 //cogigo referente a saida de informação da carta 1
    
     printf("\ncarta1 \n");
     printf("Estado: %c \n", estado1);
-    printf("Codigo: %c%s \n", estado1, codigo1);
+    printf("Codigo: %c%s \n", estado1, codigo1); // nessa linha eu uni os dois como identificador unico pois assim obrigatoriamente o codigo tera de começar com a letra que representa o estado
     printf("Nome da cidade: %s \n", cidade1);
     printf("População: %d \n", habitantes1);
-    printf("Área: %fkm² \n", area1);
-    printf("PIB: %f \n", pib1);
+    printf("Área: %.2fkm² \n", area1);
+    printf("PIB: %.2f \n", pib1);
     printf("Número de Pontos Turísticos: %d \n", turisiticos1);
+    printf("Densidade Populacional: %.2f \n", densidadepopulacional1);
+    printf("PIB per Capita: %.2f \n", pibpercapta1);
 
 //codigo referente a saida de informação da carta 2
 
@@ -89,12 +109,11 @@ int main() {
     printf("Codigo: %c%s \n", estado2, codigo2);
     printf("Nome da cidade: %s \n", cidade2);
     printf("População: %d \n", habitantes2);
-    printf("Área: %fkm² \n", area2);
-    printf("PIB: %f \n", pib2);
+    printf("Área: %.2fkm² \n", area2);
+    printf("PIB: %.2f \n", pib2);
     printf("Número de Pontos Turísticos: %d \n", turisiticos2);
-
-//fiz dessa maneira sabendo que poderia ter varias maneiras de resolver mas preferi começar fazendo o cadastro inteiro de duas cartas para depois-
-//exibilas em vez de, fazer e exibir uma por vez.
+    printf("Densidade Populacional: %.2f \n", densidadepopulacional2);
+    printf("PIB per Capita: %.2f \n", pibpercapta2);
 
     return 0;
 }
